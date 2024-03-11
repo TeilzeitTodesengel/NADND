3d0a2c1472d89dc350280b89a7372c517217e721
TeilzeitTodesengel <teilzeit.todesengel2605@gmail.com>
2024-03-11 15:41:26 +0100

    Added Audio System
    Die Start Audio kann nicht verändert werden. Jede andere schon über den Eintrag im JSON file

--- Assets/Scripts/Datatypes/StoryPart.cs
+++ Assets/Scripts/Datatypes/StoryPart.cs
@@ -6,15 +6,17 @@ public class StoryPart
     public String roomID;
     public String roomName;
     public String description;
+    public String musicID;
     public bool isFight;
     public Fight fight;
     public Abzweigung[] abzweigungen;
 
-    public StoryPart(string roomID, string roomName, string description, bool isFight, Fight fight, Abzweigung[] abzweigungen)
+    public StoryPart(string roomID, string roomName, string description, string musicID, bool isFight, Fight fight, Abzweigung[] abzweigungen)
     {
         this.roomID = roomID;
         this.roomName = roomName;
         this.description = description;
+        this.musicID = musicID;
         this.isFight = isFight;
         this.fight = fight;
         this.abzweigungen = abzweigungen;
